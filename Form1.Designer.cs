8a52c5a9d37683ccd80613dc3aef48cdd09ffe9d
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-01-26 10:42:40 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace MathApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioAddition = new System.Windows.Forms.RadioButton();
+            this.radioSubtraction = new System.Windows.Forms.RadioButton();
+            this.radioMultiplication = new System.Windows.Forms.RadioButton();
+            this.radioDivision = new System.Windows.Forms.RadioButton();
+            this.radioRemainder = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioAddition
+            // 
+            this.radioAddition.AutoSize = true;
+            this.radioAddition.Checked = true;
+            this.radioAddition.Location = new System.Drawing.Point(12, 42);
+            this.radioAddition.Name = "radioAddition";
+            this.radioAddition.Size = new System.Drawing.Size(72, 19);
+            this.radioAddition.TabIndex = 3;
+            this.radioAddition.TabStop = true;
+            this.radioAddition.Text = "+ Add to";
+            this.radioAddition.UseVisualStyleBackColor = true;
+            // 
+            // radioSubtraction
+            // 
+            this.radioSubtraction.AutoSize = true;
+            this.radioSubtraction.Location = new System.Drawing.Point(113, 41);
+            this.radioSubtraction.Name = "radioSubtraction";
+            this.radioSubtraction.Size = new System.Drawing.Size(106, 19);
+            this.radioSubtraction.TabIndex = 4;
+            this.radioSubtraction.Text = "- Subtract from";
+            this.radioSubtraction.UseVisualStyleBackColor = true;
+            // 
+            // radioMultiplication
+            // 
+            this.radioMultiplication.AutoSize = true;
+            this.radioMultiplication.Location = new System.Drawing.Point(12, 70);
+            this.radioMultiplication.Name = "radioMultiplication";
+            this.radioMultiplication.Size = new System.Drawing.Size(93, 19);
+            this.radioMultiplication.TabIndex = 5;
+            this.radioMultiplication.Text = "* Multiply by";
+            this.radioMultiplication.UseVisualStyleBackColor = true;
+            // 
+            // radioDivision
+            // 
+            this.radioDivision.AutoSize = true;
+            this.radioDivision.Location = new System.Drawing.Point(113, 70);
+            this.radioDivision.Name = "radioDivision";
+            this.radioDivision.Size = new System.Drawing.Size(82, 19);
+            this.radioDivision.TabIndex = 6;
+            this.radioDivision.Text = "/ Divide by";
+            this.radioDivision.UseVisualStyleBackColor = true;
+            // 
+            // radioRemainder
+            // 
+            this.radioRemainder.AutoSize = true;
+            this.radioRemainder.Location = new System.Drawing.Point(214, 69);
+            this.radioRemainder.Name = "radioRemainder";
+            this.radioRemainder.Size = new System.Drawing.Size(113, 19);
+            this.radioRemainder.TabIndex = 7;
+            this.radioRemainder.Text = "% Get remainder";
+            this.radioRemainder.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 24);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(118, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(363, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 22);
+            this.label2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Numeric Value";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_OnTextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Numeric Value";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_OnTextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(582, 185);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioRemainder);
+            this.Controls.Add(this.radioDivision);
+            this.Controls.Add(this.radioMultiplication);
+            this.Controls.Add(this.radioSubtraction);
+            this.Controls.Add(this.radioAddition);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(598, 224);
+            this.MinimumSize = new System.Drawing.Size(598, 224);
+            this.Name = "Form1";
+            this.Text = "Jeffrey\'s Math App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioAddition;
+        private System.Windows.Forms.RadioButton radioSubtraction;
+        private System.Windows.Forms.RadioButton radioMultiplication;
+        private System.Windows.Forms.RadioButton radioDivision;
+        private System.Windows.Forms.RadioButton radioRemainder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+
+/* References:
+ * 
+ * Free icons and images obtained from
+ * http://clipart-library.com/
+ * https://icon-icons.com/
+ * 
+ */
+
+namespace MathApp
+{
+    public partial class Form1 : Form
+    {
+        bool TextBox1Valid { get; set; }
+        bool TextBox2Valid { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Handlers
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            TextBox1Valid = false;
+            TextBox2Valid = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float textBox1Number = float.Parse(textBox1.Text);
+            float textBox2Number = float.Parse(textBox2.Text);
+
+            if (radioAddition.Checked)
+                label1.Text = GetAdditionAnswer(textBox1Number, textBox2Number);
+            else if (radioSubtraction.Checked)
+                label1.Text = GetSubtractionAnswer(textBox1Number, textBox2Number);
+            else if (radioMultiplication.Checked)
+                label1.Text = GetMultiplicationAnswer(textBox1Number, textBox2Number);
+            else if (radioDivision.Checked)
+            {
+                if (textBox2Number == 0)
+                {
+                    label2.Text = "Can't divide by zero.";
+                    label1.Text = "";
+                }
+                else
+                {
+                    label1.Text = GetDivisionAnswer(textBox1Number, textBox2Number);
+                }
+            }
+            else if (radioRemainder.Checked)
+                label1.Text = GetRemainderAnswer(textBox1Number, textBox2Number);
+        }
+
+        void textBox1_OnTextChanged(object sender, EventArgs e)
+        {
+            TextBox1Valid = ValidateTextBox(textBox1);
+            button1.Enabled = TextBox1Valid && TextBox2Valid;
+        }
+
+        void textBox2_OnTextChanged(object sender, EventArgs e)
+        {
+            TextBox2Valid = ValidateTextBox(textBox2);
+            button1.Enabled = TextBox1Valid && TextBox2Valid;
+        }
+
+        #endregion
+
+        #region Math Methods
+
+        string GetAdditionAnswer(float a, float b) => (a + b).ToString();
+
+        string GetSubtractionAnswer(float a, float b) => (b - a).ToString();
+
+        string GetMultiplicationAnswer(float a, float b) => (a * b).ToString();
+
+        string GetDivisionAnswer(float a, float b) => (a / b).ToString();
+
+        string GetRemainderAnswer(float a, float b) => (a % b).ToString();
+
+        #endregion
+
+        #region Validation Methods
+
+        bool ValidateTextBox(TextBox textBox)
+        {
+            if (textBox.Text == "")
+            {
+                label2.Text = "";
+                return false;
+            }
+            else if (!CanParseToFloat(textBox.Text))
+            {
+                label2.Text = "Numeric values only! 😡";
+                return false;
+            }
+            else
+            {
+                label2.Text = "";
+                return true;
+            }
+        }
+
+        bool CanParseToFloat(string text) => float.TryParse(text, out float _);
+
+        #endregion
+    }
+}
